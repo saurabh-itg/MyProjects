6ad4257a691b71618fd08404b842c2838c36df35
Saurabh joshi <saurabh.itg@gmail.com>
2023-10-11 10:20:38 +0530

    Add files via upload

---
+++ random_password_generator.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 10 16:53:55 2023
+
+@author: OFA2KOR
+"""
+import random
+letters=['A','B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' , 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y','z']
+num=['1','2','3','4','5','6','7','8','9','0']
+symbols=['@','#','$','%','&','*','?']        
+print("Welcome to PyPassword generator")
+n_letters=int(input("How many letters you want in the password"))
+n_symbols=int(input("How many symbols you want in the password"))
+n_nums=int(input("How many numbers you want in the password"))
+random_letters=random.sample(letters,n_letters)
+p_letters=''.join(random_letters)
+#print("randomly selected letters",random_letters)
+random_symbols=random.sample(symbols,n_symbols)
+p_symbols=''.join(random_symbols)
+#print("randomly selected symbols",random_symbols)
+random_nums=random.sample(num,n_nums)
+p_nums=''.join(random_nums)
+#print("randomly selected numbers",random_nums)
+password= p_letters+p_symbols+p_nums
+pass_string = list(password)
+random.shuffle(pass_string)
+shuffled_string = ''.join(pass_string)
+print("Your password is  :", shuffled_string)
